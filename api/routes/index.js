7bfd06e5fa9566e176139574c83717c7457b0663
KillianMrg <killian.morge@gmail.com>
2021-05-18 15:35:39 +0200

    Setup backend and db

--- api/routes/index.js
+++ api/routes/index.js
@@ -1,9 +1,25 @@
 var express = require('express');
 var router = express.Router();
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+const userRoute = require('./user');
+
+router.get('/', async (req, res) => {
+    res.status(200).json({
+        name   : 'API', 
+        version: '1.0', 
+        status : 200, 
+        message: 'Bienvenue sur medichome !'
+    });
 });
+router.get('/api', async (req, res) => {
+  res.status(200).json({
+      name   : 'API', 
+      version: '1.0', 
+      status : 200, 
+      message: 'Bienvenue sur l\'API !'
+  });
+});
+
+router.use('/api/users', userRoute);
 
 module.exports = router;

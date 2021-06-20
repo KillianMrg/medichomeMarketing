1d24864592d8e2b2bf385eeb3cc8e5469c4e714a
KillianMrg <killian.morge@gmail.com>
2021-06-21 00:44:07 +0200

    remove auth

--- api/routes/instagram.js
+++ api/routes/instagram.js
@@ -1,6 +1,5 @@
 var express = require('express');
 var router = express.Router();
-var authRouter = require('./auth');
 var postController = require('../service/instagram/post');
 var saveController = require('../service/instagram/save');
 

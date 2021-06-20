0783624c62cf5bbd3f539f95507ab052c3ca6a98
KillianMrg <killian.morge@gmail.com>
2021-06-21 00:38:29 +0200

    routes

--- api/routes/mail.js
+++ api/routes/mail.js
@@ -4,4 +4,10 @@ const mailRouter = require('../service/mail/mail');
 
 router.post('/send', mailRouter.sendEmails);
 
+router.post('/create', mailRouter.createMail);
+router.get('/getall', mailRouter.readMails);
+router.post('/getbyid', mailRouter.readMailById);
+router.post('/update', mailRouter.updateMail);
+router.post('/delete', mailRouter.deleteMail);
+
 module.exports = router;
